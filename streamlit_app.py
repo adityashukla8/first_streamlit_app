ed0c4727139122a743e29c73f34146d6af270b76
adityashukla8 <53909071+adityashukla8@users.noreply.github.com>
2022-09-16 18:05:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,4 @@ import streamlit
 
 streamlit.title('I love animals!')
 streamlit.header('Especially Cats! 😻')
+streamlit.text('No offence to dogs tho, nor snakes. Not elephants for that matter 🐶🐍🐘')
