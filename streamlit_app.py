2b2be491b806e48fc119dbc286d14b9ddb494261
adityashukla8 <53909071+adityashukla8@users.noreply.github.com>
2022-09-16 18:08:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
+import pandas
 
 streamlit.title('I love animals!')
 streamlit.header('Especially Cats! 😻')
 streamlit.text('No offence to dogs tho, nor snakes. Not elephants for that matter 🐶🐍🐘')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
