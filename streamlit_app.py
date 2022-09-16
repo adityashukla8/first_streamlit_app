21dbf0912d86e98d28489c54a143bb0f15c537f4
adityashukla8 <53909071+adityashukla8@users.noreply.github.com>
2022-09-16 18:20:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ streamlit.header('Especially Cats! 😻')
 streamlit.text('No offence to dogs tho, nor snakes. Not elephants for that matter 🐶🐍🐘')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruits')
 
 streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
